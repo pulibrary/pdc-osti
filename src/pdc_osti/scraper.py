78bcb87f39c36ca847266dd354402813c2e09ea4
Chun Ly <cly@pppl.gov>
2022-10-12 13:58:46 -0700

    Add black and flake8 support for formatting and linting (#15)
    * Add black and flake8 #11
    
     - Update modules
     - Add GitHub actions for linting/formatting
     - Define DATASPACE_URI for simplicity
    
    * Fix typo in GitHub actions black path
    
    * Refactor DSPACE_ID as common constant
    
    * Force to py3.9 for consistency
    
     - Changes to pass black CI
    
    * Fix flake8 CI
    
    * Set black and flake8 line length to 88
    
    * Minor README.md changes

--- src/pdc_osti/scraper.py
+++ src/pdc_osti/scraper.py
@@ -8,31 +8,31 @@ import pandas as pd
 
 from os.path import join as pjoin
 
-DSPACE_ID = 'DSpace ID'
+from . import DATASPACE_URI, DSPACE_ID
 
 # NOTE: The Dataspace REST API can now support requests from handles.
 #  Shifting this scrape to collection handles instead of IDs may make
 #  this script clearer and easier to change if necessary.
 PPPL_COLLECTIONS = {
-    'NSTX': 1282,
-    'NSTX-U': 1304,
-    'Stellarators': 1308,
-    'Plasma Science & Technology': 1422,
-    'Theory and Computation': 2266,
-    'ITER and Tokamaks PPPL Collaborations': 3378,
-    'Theory': 3379,
-    'Computational Science PPPL Collaborations': 3380,
-    'Engineering Research': 3381,
-    'ESH Technical Reports': 3382,
-    'IT PPPL Collaborations': 3383,
-    'Advanced Projects Other Projects': 3386,
-    'Advanced Projects System Studies': 1309,
+    "NSTX": 1282,
+    "NSTX-U": 1304,
+    "Stellarators": 1308,
+    "Plasma Science & Technology": 1422,
+    "Theory and Computation": 2266,
+    "ITER and Tokamaks PPPL Collaborations": 3378,
+    "Theory": 3379,
+    "Computational Science PPPL Collaborations": 3380,
+    "Engineering Research": 3381,
+    "ESH Technical Reports": 3382,
+    "IT PPPL Collaborations": 3383,
+    "Advanced Projects Other Projects": 3386,
+    "Advanced Projects System Studies": 1309,
 }
 
 PPPL_COMMUNITY_ID = 346
 
-# All possible prefix
-REGEX_DOE = r"^(DE|AC|SC|FC|FG|AR|EE|EM|FE|NA|NE)"  # https://regex101.com/r/SxNHJg
+# All possible prefix: https://regex101.com/r/SxNHJg
+REGEX_DOE = r"^(DE|AC|SC|FC|FG|AR|EE|EM|FE|NA|NE)"
 REGEX_DOE_SUB = "^(DE)+(-?)"  # https://regex101.com/r/NsZbRJ
 REGEX_FUNDING = r"\b(?:[A-Z0-9\/\-]{6,})"  # https://regex101.com/r/4fNYVm
 REGEX_BARE_DOE = re.compile(
@@ -40,15 +40,15 @@ REGEX_BARE_DOE = re.compile(
 )  # https://regex101.com/r/2s3dA3
 
 REPLACE_DICT = {
-    '- ': '-',  # Extra white space inside DoE grant
-    'AC02 ': 'AC02-',  # Missing hyphen
-    'AC-02': 'AC02',  # Extra hyphen
-    'SC-0': 'SC0',  # Extra hyphen for Office of Science grants
-    'DC': 'DE',  # Common typo
-    'DE ': 'DE',  # Extra white space
-    'DOE-': 'DE',  # Proper prefix
-    'DOE ': '',  # Extra DOE
-    'DOE': '',  # Remove DOE if still present
+    "- ": "-",  # Extra white space inside DoE grant
+    "AC02 ": "AC02-",  # Missing hyphen
+    "AC-02": "AC02",  # Extra hyphen
+    "SC-0": "SC0",  # Extra hyphen for Office of Science grants
+    "DC": "DE",  # Common typo
+    "DE ": "DE",  # Extra white space
+    "DOE-": "DE",  # Proper prefix
+    "DOE ": "",  # Extra DOE
+    "DOE": "",  # Remove DOE if still present
 }
 
 
@@ -74,12 +74,17 @@ class Scraper:
     :ivar to_upload: JSON output file containing metadata for OSTI upload
     :ivar redirects: JSON output file containing DOI redirects
     """
-    def __init__(self, data_dir='data', osti_scrape='osti_scrape.json',
-                 dspace_scrape='dspace_scrape.json',
-                 entry_form_full_path='entry_form.tsv',
-                 form_input_full_path='form_input.tsv',
-                 to_upload='dataset_metadata_to_upload.json',
-                 redirects='redirects.json'):
+
+    def __init__(
+        self,
+        data_dir="data",
+        osti_scrape="osti_scrape.json",
+        dspace_scrape="dspace_scrape.json",
+        entry_form_full_path="entry_form.tsv",
+        form_input_full_path="form_input.tsv",
+        to_upload="dataset_metadata_to_upload.json",
+        redirects="redirects.json",
+    ):
 
         self.osti_scrape = pjoin(data_dir, osti_scrape)
         self.dspace_scrape = pjoin(data_dir, dspace_scrape)
@@ -100,17 +105,23 @@ class Scraper:
         existing_datasets = []
 
         for page in range(MAX_PAGE_COUNT):
-            r = requests.get(f'https://www.osti.gov/dataexplorer/api/v1/records?site_ownership_code=PPPL&page={page}')
+            url = (
+                "https://www.osti.gov/dataexplorer/api/v1/records?"
+                f"site_ownership_code=PPPL&page={page}"
+            )
+            r = requests.get(url)
             j = json.loads(r.text)
             if len(j) != 0:
                 existing_datasets.extend(j)
             else:
-                print(f'Pulled {len(existing_datasets)} records from OSTI.')
+                print(f"Pulled {len(existing_datasets)} records from OSTI.")
                 break
         else:
-            raise BaseException("Didn't reach the final page of OSTI! Increase the variable MAX_PAGE_COUNT")
+            raise BaseException(
+                "Didn't reach the final OSTI page! Increase the MAX_PAGE_COUNT"
+            )
 
-        with open(self.osti_scrape, 'w') as f:
+        with open(self.osti_scrape, "w") as f:
             json.dump(existing_datasets, f, indent=4)
 
     def get_dspace_metadata(self):
@@ -122,34 +133,36 @@ class Scraper:
         all_items = []
 
         for c_name, c_id in PPPL_COLLECTIONS.items():
-            url = f'https://dataspace.princeton.edu/rest/collections/{c_id}/items?expand=metadata'
+            url = f"{DATASPACE_URI}/rest/collections/{c_id}/items?expand=metadata"
             r = requests.get(url)
             j = json.loads(r.text)
             all_items.extend(j)
 
         # Confirm that all collections were included
-        url_all = f"https://dataspace.princeton.edu/rest/communities/{PPPL_COMMUNITY_ID}"
+        url_all = f"{DATASPACE_URI}/rest/communities/{PPPL_COMMUNITY_ID}"
         r = requests.get(url_all)
-        print('countItems: ', json.loads(r.text)['countItems'])
-        print('all_items: ', len(all_items))
-        assert json.loads(r.text)['countItems'] == len(all_items),\
-            ("The number of items in the PPPL community does not equal the "
-             "number of items collected. Review the list of collections we "
-             "search through (variable COLLECTION_IDS) and ensure that all "
-             "PPPL collections are included. Or write a recursive function "
-             "to prevent this from happening again.")
+        print("countItems: ", json.loads(r.text)["countItems"])
+        print("all_items: ", len(all_items))
+        assert json.loads(r.text)["countItems"] == len(all_items), (
+            "The number of items in the PPPL community does not equal the "
+            "number of items collected. Review the list of collections we "
+            "search through (variable COLLECTION_IDS) and ensure that all "
+            "PPPL collections are included. Or write a recursive function "
+            "to prevent this from happening again."
+        )
 
-        print(f'Pulled {len(all_items)} records from DSpace.')
-        with open(self.dspace_scrape, 'w') as f:
+        print(f"Pulled {len(all_items)} records from DSpace.")
+        with open(self.dspace_scrape, "w") as f:
             json.dump(all_items, f, indent=4)
 
     def get_unposted_metadata(self):
-        """Compare OSTI and DataSpace JSON to identify records to be uploaded"""
+        """Compare OSTI and DataSpace JSON to identify records for uploading"""
+
         def get_handle(doi, redirects_j):
             if doi not in redirects_j:
                 r = requests.get(doi)
                 assert r.status_code == 200, f"Error parsing DOI: {doi}"
-                handle = r.url.split('handle/')[-1]
+                handle = r.url.split("handle/")[-1]
                 redirects_j[doi] = handle
                 return handle
             else:
@@ -164,26 +177,32 @@ class Scraper:
 
         # Find handles in DSpace whose handles aren't linked in OSTI's DOIs
         # HACK: returning proper DOI while also updating redirects_j
-        osti_handles = [get_handle(record['doi'], redirects_j)
-                        for record in osti_j]
+        osti_handles = [get_handle(record["doi"], redirects_j) for record in osti_j]
 
         to_be_published = []
         for dspace_record in dspace_j:
-            if dspace_record['handle'] not in osti_handles:
+            if dspace_record["handle"] not in osti_handles:
                 to_be_published.append(dspace_record)
 
-        with open(self.to_upload, 'w') as f:
+        with open(self.to_upload, "w") as f:
             json.dump(to_be_published, f, indent=4)
-        with open(self.redirects, 'w') as f:
+        with open(self.redirects, "w") as f:
             json.dump(redirects_j, f, indent=4)
 
         # Check for records in OSTI but not DSpace
-        dspace_handles = [record['handle'] for record in dspace_j]
-        errors = [record for record in osti_j if redirects_j[record['doi']] not in dspace_handles]
+        dspace_handles = [record["handle"] for record in dspace_j]
+        errors = [
+            record
+            for record in osti_j
+            if redirects_j[record["doi"]] not in dspace_handles
+        ]
         if len(errors) > 0:
-            print(f"The following records were found on OSTI but not in DSpace (that" + 
-                  " shouldn't happen). If they closely resemble records we are about to" +
-                  " upload, please remove those records from from the upload process.")
+            print(
+                "The following records were found on OSTI but not in DSpace "
+                "(that shouldn't happen). If they closely resemble records we "
+                "are about to upload, please remove those records from from "
+                "the upload process."
+            )
             for error in errors:
                 print(f"\t{error['title']}")
 
@@ -196,30 +215,39 @@ class Scraper:
             to_upload_j = json.load(f)
 
         df = pd.DataFrame()
-        df[DSPACE_ID] = [item['id'] for item in to_upload_j]
-        df['Issue Date'] = [[m['value'] for m in item['metadata'] if m['key'] == 'dc.date.issued'][0]
-                            for item in to_upload_j]
-        df['Title'] = [item['name'] for item in to_upload_j]
-        df['Author'] = [';'.join([m['value'] for m in item['metadata'] if
-                                  m['key'] == 'dc.contributor.author'])
-                        for item in to_upload_j]
-        df['Dataspace Link'] = ["https://dataspace.princeton.edu/handle/" + item['handle']
-                                for item in to_upload_j]
+        df[DSPACE_ID] = [item["id"] for item in to_upload_j]
+        df["Issue Date"] = [
+            [m["value"] for m in item["metadata"] if m["key"] == "dc.date.issued"][0]
+            for item in to_upload_j
+        ]
+        df["Title"] = [item["name"] for item in to_upload_j]
+        df["Author"] = [
+            ";".join(
+                [
+                    m["value"]
+                    for m in item["metadata"]
+                    if m["key"] == "dc.contributor.author"
+                ]
+            )
+            for item in to_upload_j
+        ]
+        df["Dataspace Link"] = [
+            f"{DATASPACE_URI}/handle/{item['handle']}" for item in to_upload_j
+        ]
 
         # Retrieve funding data
         funding_text_list = [
             [
-                m['value']
-                for m in item['metadata']
-                if m['key'] == 'dc.contributor.funder'
+                m["value"]
+                for m in item["metadata"]
+                if m["key"] == "dc.contributor.funder"
             ]
             for item in to_upload_j
         ]
 
         # Generate lists of lists per each dc.contributor.funder entry
         funding_result = [
-            list(filter(None, map(get_funder, f_list)))
-            for f_list in funding_text_list
+            list(filter(None, map(get_funder, f_list))) for f_list in funding_text_list
         ]
         funding_result_simple = [  # All grants for each DSpace record
             ";".join([";".join(value) if value else "" for value in res])
@@ -227,26 +255,30 @@ class Scraper:
         ]
         funding_source_dict = list(map(get_doe_funding, funding_result_simple))
 
-        df['DOE Contract'] = [
+        df["DOE Contract"] = [
             ";".join(sorted(d.get("doe"))) for d in funding_source_dict
         ]
-        df['Non-DOE Contract'] = [
+        df["Non-DOE Contract"] = [
             ";".join(sorted(d.get("other"))) for d in funding_source_dict
         ]
 
         # Sponsoring organizations is always Office of Science
-        df['Sponsoring Organizations'] = "USDOE Office of Science (SC)"
+        df["Sponsoring Organizations"] = "USDOE Office of Science (SC)"
 
-        df['Datatype'] = None  # To be filled in
+        df["Datatype"] = None  # To be filled in
 
-        df = df.sort_values('Issue Date')
-        df.to_csv(self.entry_form, index=False, sep='\t')
+        df = df.sort_values("Issue Date")
+        df.to_csv(self.entry_form, index=False, sep="\t")
 
-        print(f"{df.shape[0]} unpublished records were found in the PPPL "
-              f"dataspace community that have not been registered with OSTI.")
+        print(
+            f"{df.shape[0]} unpublished records were found in the PPPL "
+            f"dataspace community that have not been registered with OSTI."
+        )
         print(f"They've been saved to the form {self.entry_form}.")
-        print("You're now expected to manually update that form and save as a "
-              "new file before running Poster.py")
+        print(
+            "You're now expected to manually update that form and save as a "
+            "new file before running Poster.py"
+        )
         for i, row in df.iterrows():
             print(f"\t{repr(row['Title'])}")
             print(f"\t\t{row['Dataspace Link']}")
@@ -256,13 +288,14 @@ class Scraper:
         Update form_input.tsv by adding new records or removing DataSpace
         records that were removed/withdrawn
 
-        In most cases, this will update form_input.tsv. This further supports CI
+        In most cases, this will update form_input.tsv. This further
+        supports CI
         """
         if os.path.exists(self.form_input):
             print(f"File exists. Will update: {self.form_input}")
 
-            entry_df = pd.read_csv(self.entry_form, index_col=DSPACE_ID, sep='\t')
-            input_df = pd.read_csv(self.form_input, index_col=DSPACE_ID, sep='\t')
+            entry_df = pd.read_csv(self.entry_form, index_col=DSPACE_ID, sep="\t")
+            input_df = pd.read_csv(self.form_input, index_col=DSPACE_ID, sep="\t")
             print("Identifying DataSpace records to add and remove ...")
             entry_id = set(entry_df.index)
             input_id = set(input_df.index)
@@ -278,9 +311,9 @@ class Scraper:
             revised_df = input_df.append(entry_df.loc[adds])
 
             # "AS" is a placeholder - not included in DataSpace metadata
-            revised_df.loc[adds, 'Datatype'] = "AS"
+            revised_df.loc[adds, "Datatype"] = "AS"
 
-            revised_df.to_csv(self.form_input, sep='\t')
+            revised_df.to_csv(self.form_input, sep="\t")
         else:
             raise FileNotFoundError(f"WARNING: {self.form_input} does not exist!")
 
@@ -301,7 +334,7 @@ def get_funder(text: str) -> list:
         text = text.replace(key, value)
 
     for hyphen in ["\u2010", "\u2013"]:
-        text = text.replace(hyphen, '-')
+        text = text.replace(hyphen, "-")
 
     base_match = re.match(REGEX_BARE_DOE, text)
     if base_match:  # DOE/FES funded but no grant number
@@ -314,10 +347,7 @@ def get_funder(text: str) -> list:
 def get_doe_funding(grant_nos: str) -> Dict[str, set]:
     """Separate DOE from other funding. Prefix DE prefix"""
 
-    grant_dict = {
-        "doe": set(),
-        "other": set(),
-    }
+    grant_dict = {"doe": set(), "other": set()}
 
     if not grant_nos:  # Empty case
         grant_dict["doe"].update(["AC02-09CH11466"])
@@ -335,6 +365,6 @@ def get_doe_funding(grant_nos: str) -> Dict[str, set]:
 def main():
     s = Scraper()
     # NOTE: It may be useful to implement a CLI command (e.g. --no-scrape) to
-    #  allow for debugging the get_unposted_metadata or generate_contract_entry_form
-    #  functions
+    #       allow for debugging the get_unposted_metadata or
+    #       generate_contract_entry_form functions
     s.run_pipeline()
