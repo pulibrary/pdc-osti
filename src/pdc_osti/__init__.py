d3cc5b4aa17cf151926fef17b55ca7cbe92f17c5
Chun Ly <cly@pppl.gov>
2023-04-04 12:27:34 -0700

    Use pppl JSON endpoint for PDC discovery
     - Change filename convention for data (poster and scraper module)

--- src/pdc_osti/__init__.py
+++ src/pdc_osti/__init__.py
@@ -5,12 +5,4 @@ LOG_NAME = "pdc-osti"
 DATASPACE_URI = "https://dataspace.princeton.edu"
 DSPACE_ID = "DSpace ID"
 
-PDC_URI = "https://datacommons.princeton.edu/discovery/catalog.json"
-PDC_QUERY = {
-    "f[community_root_name_ssi][]": "Princeton+Plasma+Physics+Laboratory",
-    "search_field": "all_fields",
-    "fl": "*",
-    "q": "",
-    "format": "json",
-    "per_page": 100,
-}
+PDC_URI = "https://datacommons.princeton.edu/discovery/pppl.json"
