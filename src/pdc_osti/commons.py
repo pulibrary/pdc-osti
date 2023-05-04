c244c779f8cf1d779ec3127e8b24018c0c784cbd
Chun Ly <cly@pppl.gov>
2023-05-04 13:51:55 -0700

    generate_contract_entry_form: Handle both DSpace and PDC
     - Add get_datacite_awards function
     - Fix typo in princeton_metadata_handle (strip ARK prefix)

--- src/pdc_osti/commons.py
+++ src/pdc_osti/commons.py
@@ -1,3 +1,7 @@
 def get_dc_value(item: dict, key: str) -> list:
     """Retrieve DublinCore (DC) metadata"""
     return [m["value"] for m in item["metadata"] if m["key"] == key]
+
+
+def get_datacite_awards(item: dict) -> list:
+    return [m["award_number"] for m in item["resource"].get("funders")]
