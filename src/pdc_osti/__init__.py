09f74dcea9787d7165339c94d6f89077a3de9dd7
Chun Ly <cly@pppl.gov>
2023-10-12 12:26:07 -0700

    Deprecate DataSpace support (#59, #60)
    * Remove smoke tests involving dspace
    * Simplify scraper module with dataspace deprecation
    * Simplify poster module with dataspace deprecation
    * Simplify commons module with dataspace deprecation
    * Remove get_handle method in Scraper
    * Removed unused DataSpace variables
    * Remove dspace metadata files
    * Remove dspace entry table
    * Remove references to DataSpace/DSpace
    * Update README.md for DataSpace/DSpace references

--- src/pdc_osti/__init__.py
+++ src/pdc_osti/__init__.py
@@ -2,8 +2,5 @@ __version__ = "0.1.0"
 
 LOG_NAME = "pdc-osti"
 
-DATASPACE_URI = "https://dataspace.princeton.edu"
-DSPACE_ID = "DSpace ID"
-
 PDC_QUERY = {"per_page": 20}
 PDC_URI = "https://pdc-discovery-prod.princeton.edu/discovery/pppl_reporting_feed.json"
--- src/pdc_osti/commons.py
+++ src/pdc_osti/commons.py
@@ -1,14 +1,10 @@
 from typing import List
 
 
-def get_ark(record: dict, princeton_source: str) -> str:
+def get_ark(record: dict) -> str:
     """Retrieves ARK (e.g., 88435/dsp012j62s808w) depending on Princeton source"""
-    if princeton_source == "dspace":
-        return record["handle"]
-    elif princeton_source == "pdc":
-        return record["resource"]["ark"].replace("ark:/", "")
-    else:
-        raise NotImplementedError
+
+    return record["resource"]["ark"].replace("ark:/", "")
 
 
 def get_author(creator: dict) -> dict:
@@ -37,23 +33,13 @@ def get_authors(record: dict) -> List[dict]:
         return [get_author(creator) for creator in creators]
 
 
-def get_dc_value(item: dict, key: str) -> list:
-    """Retrieve DublinCore (DC) metadata"""
-    return [m["value"] for m in item["metadata"] if m["key"] == key]
-
-
 def get_datacite_awards(item: dict) -> list:
     return [m["award_number"] for m in item["resource"].get("funders")]
 
 
-def get_description(item: dict, princeton_source: str) -> str:
+def get_description(item: dict) -> str:
     """Retrieve description of dataset"""
-    if princeton_source == "dspace":
-        abstract = get_dc_value(item, "dc.description.abstract")
-    elif princeton_source == "pdc":
-        abstract = [item["resource"].get("description")]
-    else:
-        raise NotImplementedError
+    abstract = [item["resource"].get("description")]
 
     if len(abstract) != 0:
         return "\n\n".join(abstract)
@@ -61,38 +47,22 @@ def get_description(item: dict, princeton_source: str) -> str:
         return ""
 
 
-def get_is_referenced_by(item: dict, princeton_source: str) -> str:
+def get_is_referenced_by(item: dict) -> str:
     """Retrieve IsReferencedBy for dataset"""
-    if princeton_source == "dspace":
+    related_objects = item["resource"].get("related_objects")
+    if related_objects:
         isreferencedby = [
-            val.split("doi.org/")[1]
-            for val in get_dc_value(item, "dc.relation.isreferencedby")
+            m["related_identifier"]
+            for m in related_objects
+            if m["relation_type"] in ["IsCitedBy", "IsReferencedBy"]
         ]
-    elif princeton_source == "pdc":
-        related_objects = item["resource"].get("related_objects")
-        if related_objects:
-            isreferencedby = [
-                m["related_identifier"]
-                for m in related_objects
-                if m["relation_type"] in ["IsCitedBy", "IsReferencedBy"]
-            ]
-        else:
-            isreferencedby = []
     else:
-        raise NotImplementedError
+        isreferencedby = []
 
     return isreferencedby
 
 
-def get_keywords(item: dict, princeton_source: str) -> str:
-    if princeton_source == "dspace":
-        keywords = get_dc_value(item, "dc.subject")
-    elif princeton_source == "pdc":
-        keywords = item["resource"].get("keywords")
-    else:
-        raise NotImplementedError
+def get_keywords(item: dict) -> str:
+    keywords = item["resource"].get("keywords")
 
-    if len(keywords) != 0:
-        return "; ".join(keywords)
-    else:
-        return ""
+    return "; ".join(keywords)
