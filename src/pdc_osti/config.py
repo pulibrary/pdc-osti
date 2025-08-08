7e15679335ad81b53fe5e8b3973a950acde08504
Chun Ly <cly@pppl.gov>
2025-08-08 14:24:04 -0700

    Remove ostiapi and dicttoxml, replace with elinkapi

--- src/pdc_osti/config.py
+++ src/pdc_osti/config.py
@@ -4,10 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """API Configuration"""
 
-    OSTI_USERNAME_TEST: str
-    OSTI_PASSWORD_TEST: str
-    OSTI_USERNAME_PROD: str
-    OSTI_PASSWORD_PROD: str
+    ELINK2_TOKEN_TEST: str
+    ELINK2_TOKEN_PROD: str
 
     class Config:
         env_file = ".env"
