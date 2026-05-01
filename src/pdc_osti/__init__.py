b780f6e5b66de805db7138babfa18526ab6c9627
Chun Ly <astro.chun@gmail.com>
2026-05-01 06:51:49 -0700

    Update PDC_URI #88

--- src/pdc_osti/__init__.py
+++ src/pdc_osti/__init__.py
@@ -3,4 +3,4 @@ __version__ = "0.1.0"
 LOG_NAME = "pdc-osti"
 
 PDC_QUERY = {"per_page": 20}
-PDC_URI = "https://pdc-discovery-prod.princeton.edu/discovery/pppl_reporting_feed.json"
+PDC_URI = "https://datacommons.princeton.edu/discovery/pppl_reporting_feed.json"
